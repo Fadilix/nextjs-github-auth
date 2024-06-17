dfa598c1ded24729548099c463d5b7a06d1e3afb
Fadilix <iamfadile2@gmail.com>
2024-06-17 17:10:19 +0000

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,20 @@
 import AuthButton from "@/components/AuthButton";
 import SecretLink from "@/components/SecretLink";
-import { signIn, signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex items-center justify-between flex-col">
+      <div className="flex items-center justify-between flex-col w-full">
 
-        <nav className="flex items-center justify-between w-screen px-40">
+        <nav className="flex items-center justify-between w-full px-40">
           <Link href="/">Home</Link>
-          <Link href="/">Home</Link>
-          <Link href="/">Home</Link>
+          <Link href="/">About</Link>
+          <Link href="/">Contact</Link>
           <SecretLink />
         </nav>
-        <div>
+
+        <div className="mt-8">
           <AuthButton />
         </div>
       </div>
